99dcb0718e47ca071f2b4ff9240b89ff04b7a80a
MurryPuppins <cjcarey314@gmail.com>
2021-06-23 17:23:23 -0400

    - Updated guppyBot and clutchBot to reflect current changes - Fixed Github repo issues

--- clutchGamingBot/clutchBot.py
+++ clutchGamingBot/clutchBot.py
@@ -1,122 +1,86 @@
 import discord
+import traceback
+import os
 import asyncio
 from discord.ext import commands
 import random
 from mcstatus import MinecraftServer
-import json
+import sqlite3
 
-async def get_prefix(bot, message):
-    if not message.guild:
-        return commands.when_mentioned_or(">")(bot, message)
+intents = discord.Intents.default()
+intents.members = True
 
-    with open("../vibeBotOfficial/prefixes.json", 'r') as f:
-        prefixes = json.load(f)
-
-    if str(message.guild.id) not in prefixes:
-        return commands.when_mentioned_or(">")(bot, message)
-
-    prefix = prefixes[str(message.guild.id)]
-    return commands.when_mentioned_or(prefix)(bot, message)
-
-"""
-async def getPrefix(client, message):
-    with open('prefixes.json', 'r') as f:
-        prefixes = json.load(f)
-
-    return prefixes[str(message.guild.id)]"""
-
-bot = commands.Bot(command_prefix=get_prefix)
-"bot = commands.Bot(command_prefix='<', case_insensitive=True)"
+bot = commands.Bot(command_prefix='<', intents=intents)
+bot.remove_command("help")
 
 count = 0
+client = discord.Client()
+bot_name = 'Anti-Gabe Bot'
+smp = MinecraftServer("192.168.1.13", 25571)
+modded = MinecraftServer("192.168.1.13", 25570)
 
-#client = discord.Client()
-bot.remove_command("help")
-rankname = 'Member'
-smp = MinecraftServer("mc.clutchgaming.xyz", 25583)
-modded = MinecraftServer("mod.clutchgaming.xyz", 25569)
-hypixel = MinecraftServer("mc.hypixel.net", 25565)
+curate_list = 'nsfw, gifs, words'
+con = sqlite3.connect('counter.db')
+cur = con.cursor()
 
-
-def vibeInc():
-    global count
-    count += 1
-
-# When bot joins guild, assign default prefix
-@bot.event
-async def on_guild_join(guild):
-    with open('../vibeBotOfficial/prefixes.json', 'r') as f:
-        prefixes = json.load(f)
-
-    prefixes[str(guild.id)] = '>'
-
-    with open('../vibeBotOfficial/prefixes.json', 'w') as f:
-        json.dump(prefixes, f, indent=4)
-
-@bot.event
-async def on_guild_remove(guild):
-    with open('../vibeBotOfficial/prefixes.json', 'r') as f:
-        prefixes = json.load(f)
-
-    prefixes.pop(str(guild.id))
-
-    with open('../vibeBotOfficial/prefixes.json', 'w') as f:
-        json.dump(prefixes, f, indent=4)
-
-@bot.command()
-async def prefix(ctx, prefix):
-    with open('../vibeBotOfficial/prefixes.json', 'r') as f:
-        prefixes = json.load(f)
-
-    prefixes[str(ctx.guild.id)] = prefix
-
-    with open('../vibeBotOfficial/prefixes.json', 'w') as f:
-        json.dump(prefixes, f, indent=4)
-
-    await ctx.send(f'Prefix changed to: {prefix}')
 
 @bot.event
 async def on_ready():
-    guilds = bot.guilds
-    with open('../vibeBotOfficial/prefixes.json', 'r') as f:
-        curr = json.load(f)
-    for g in guilds:
-        if g.id in curr:
-            return
-        else:
-            curr[str(g.id)] = '<'
-    with open('../vibeBotOfficial/prefixes.json', 'w') as f:
-        json.dump(curr, f, indent=4)
-    print('Successfully booted Clutchbot')
+    #if db not in db.get_tables():
+    #    db.add_table('counter', column1='INT')
+    print('Successfully booted ' + bot_name)
     await bot.change_presence(activity=discord.Game(name='1+1=3!'))
     #await dq_ping()
 
-# Helper function to append suggestion to suggestion file
-async def suggestadd(sug):
-    with open('suggestions.txt', 'a') as f:
-        f.write(sug + '\n')
-        f.close()
-    return
 
-# Suggestion command to handle suggestions for vibes from players
 @bot.command()
-async def vibesug(ctx, arg):
-    await suggestadd(arg)
-    await ctx.send('Suggestion has been noted!')
+async def options(ctx):
+    embed = discord.Embed(
+        title='Vibe Curation',
+        description='Curate your vibe list to your likings!',
+        colour=discord.Colour.purple()
+    )
+    embed.set_author(name=bot_name)
+    embed.add_field(name='Words', value='All vibes in worded format', inline=False)
+    embed.add_field(name='Gifs', value='All vibes in gif format', inline=False)
+    embed.add_field(name='NSFW', value='Non-graphic language and adult-humor', inline=False)
+    embed.add_field(name='Memes', value='Memes Galore!', inline=False)
+    await ctx.send(embed=embed)
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    if hasattr(ctx.command, 'on_error'):
+        return
+    if isinstance(error, commands.CommandNotFound):
+        return
+
+
+@bot.command()
+@commands.has_role('Admin')
+async def restart_mod(ctx):
+    os.system('tmux send -t 0:modded.1 stop ENTER')
+    await asyncio.sleep(10)
+    os.system('tmux send -t 0:modded.1 C-c')
+    os.system('tmux send -t 0:modded.1 ./start.sh ENTER')
+    await ctx.send("Sent restart command to modded server!")
+
 
 @bot.command()
 async def role(ctx, arg):
     author = ctx.message.author
-    if arg in ("mc", "MC", "minecraft", "Minecraft"):
-        await author.add_roles(discord.utils.get(author.guild.roles, name="Minecraft"))
-        await ctx.send("Minecraft role has been applied!")
+    if arg.lower() == "amongus":
+        await author.add_roles(discord.utils.get(author.guild.roles, name="Among Us"))
+        await ctx.send("Among Us role has been applied!")
         return
-    if arg in ("Rust", "rust"):
+    if arg.lower() == "rust":
         await author.add_roles(discord.utils.get(author.guild.roles, name="Rust"))
         await ctx.send("Rust role has been applied!")
-    if arg in ("bot", "Bots", "Bot", "bots"):
+        return
+    if arg.lower() == "bots":
         await author.add_roles(discord.utils.get(author.guild.roles, name="Bots"))
         await ctx.send("Bot role has been applied!")
+        return
     else:
         await ctx.send("Role request not recognized!")
 
@@ -129,17 +93,33 @@ async def dq_ping():
         await channel.send('<@400286398575280158>, your vibe has been checked! <:patrickpingthrow:643614030933786636>')
         await asyncio.sleep(86400)
 """
+
+
 @bot.event
 async def on_member_join(member):
-    role = discord.utils.get(member.guild.roles, name=rankname)
-    await member.add_roles(role)
-    channel = client.get_channel(666143692159188993)
-    await channel.send(str(member) + ' has been autoroled!')
+    await bot.get_channel(347239762798837761).send("Welcome {0.mention} to ClutchGaming, we hope you enjoy your time here!".format(member))
+    await member.add_roles(discord.utils.get(member.guild.roles, name="Member"))
 
-def vibeload():
-    with open('vibe.txt') as f:
-        lines = [line.rstrip() for line in f]
-    return lines
+
+@bot.event
+async def on_member_remove(member):
+    await bot.get_channel(347239762798837761).send("Goodbye {0} {1}".format(member, bot.get_emoji(644734494502420490)))
+
+
+@bot.command()
+async def links(ctx):
+    embed = discord.Embed(
+        title='Links',
+        description='All links relevant to ClutchGaming!',
+        colour=discord.Colour.purple()
+    )
+    embed.set_author(name=bot_name)
+    embed.add_field(name='Discord', value='https://discord.gg/BzFpan8', inline=False)
+    embed.add_field(name='Website', value='https://store.clutchgaming.xyz', inline=False)
+    embed.add_field(name='Survival', value='mc.clutchgaming.xyz', inline=False)
+    embed.add_field(name='Modded', value='mod.clutchgaming.xyz', inline=False)
+    await ctx.send(embed=embed)
+
 
 # Help command for commands relating to Vibe Bot
 @bot.command()
@@ -149,70 +129,51 @@ async def help(ctx):
         description='All commands relevant to Vibe Bot!',
         colour=discord.Colour.dark_magenta()
     )
-    embed.set_author(name='Vibe Bot')
+    embed.set_author(name=bot_name)
     embed.add_field(name='<vibe', value='Checks your vibe!', inline=False)
     embed.add_field(name='<ping', value='Ping to test status of Vibe Bot!', inline=False)
     embed.add_field(name='<fetchbutter', value='Fetches you some butter!', inline=False)
     embed.add_field(name='<role <role>', value='Get a role to get notifications of our updates! Available roles: Minecraft, Rust and Bots', inline=False)
-    embed.add_field(name='<vibesug ""', value='Have a vibe suggestion? Run the command *with* quotes! E.g > <vibesug "This bot is gucci gang"',
-                    inline=False)
+    embed.add_field(name='<vibesug msg', value='Have a vibe suggestion? Run the command with your suggestion afterwards! E.g > <vibesug This bot is gucci gang', inline=False)
     await ctx.send(embed=embed)
 
-@bot.command()
-async def links(ctx):
-    embed = discord.Embed(
-        title='Links',
-        description='All links relevant to ClutchGaming!',
-        colour=discord.Colour.purple()
-    )
-    embed.set_author(name='Vibe Bot')
-    embed.add_field(name='Website', value='https://store.clutchgaming.xyz', inline=False)
-    embed.add_field(name='Disboard', value='https://disboard.org/server/230087276967886850', inline=False)
-    embed.add_field(name='Survival', value='mc.clutchgaming.xyz', inline=False)
-    embed.add_field(name='Modded', value='mod.clutchgaming.xyz', inline=False)
-    await ctx.send(embed=embed)
-
-@bot.command()
-async def iwannadateyou(ctx):
-    await ctx.send('Wanna date? Here is my number: (605)-475-6968')
-
-@bot.command()
-async def vibe(ctx):
-    await ctx.send(random.choice(lines))
-    await vibeInc()
-
-@bot.command()
-async def fetchbutter(ctx):
-    await ctx.send(':butter:')
-
-@bot.command()
-async def help(ctx):
-    await ctx.send('Just hold your horses, I am still in experimental mode!')
 
 @bot.command()
 async def ping(ctx):
+    #await ctx.send("test")
     embed = discord.Embed(
         title='ClutchGaming',
         description='Health and Status of our Servers!',
         colour=discord.Colour.green()
     )
-    embed.set_author(name='Vibe Bot')
+    embed.set_author(name=bot_name)
     embed.add_field(name='Survival', value='Ping = {0} ms'.format(smp.ping()), inline=False)
     embed.add_field(name='Modded', value='Ping = {0} ms'.format(modded.ping()), inline=False)
-    embed.add_field(name='Hypixel', value='Ping = {0} ms'.format(hypixel.ping()), inline=False)
+    #embed.add_field(name='Hypixel', value='Ping = {0}'.format(hypixel.ping()), inline=False)
     await ctx.send(embed=embed)
 
-"""
+
 @bot.command()
-async def ping(ctx):
-    role = discord.utils.get(ctx.guild.roles, name='Admin')
-    if role in ctx.author.roles:
-        await ctx.send('Pong!')
-    else:
-        await ctx.send('You need the ' + role.name + 'role!')"""
+async def iwannadateyou(ctx):
+    await ctx.send('Wanna date? Here is my number: (605)-475-6968')
 
 
-lines = vibeload()
-print(lines)
-bot.load_extension('cogs.prefix')
-bot.run('MzM1NjM3NjQ5MDMxMTY4MDAw.Xl3yMw.R2OTGdH49JDgMSTaKmmTs6vGW2w')
+# For testing purposes, specifically pertaining to database modifications
+@bot.command()
+async def database_commands(ctx):
+    if ctx.message.author.id == 169554013837066241:
+        cur.execute("UPDATE vibecount SET count = count+1")
+        cur.execute("SELECT count FROM vibecount")
+        print(cur.fetchone()[0])
+        con.commit()
+        await ctx.send("Cmd run gut")
+        return
+    await ctx.send("No sir, yer neer to be murr")
+
+
+@bot.command()
+async def fetchbutter(ctx):
+    await ctx.send(':butter:')
+
+
+bot.run('')
